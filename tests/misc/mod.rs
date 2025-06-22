74854e9849a0a474ea29c34080d1032a64175b1a
Matt Ehrnschwender <matthewe2020@gmail.com>
2025-06-22 11:17:59 -0400

    test: adjust tests for '--no-merge-groups'
    Add test case for '--no-merge-groups'. Refactor existing tests to merge
    by default

--- tests/misc/mod.rs
+++ tests/misc/mod.rs
@@ -1,4 +1,12 @@
 use boflink::linker::LinkerTargetArch;
+use object::{
+    Object,
+    coff::CoffFile,
+    pe::{
+        IMAGE_SCN_CNT_CODE, IMAGE_SCN_CNT_INITIALIZED_DATA, IMAGE_SCN_MEM_EXECUTE,
+        IMAGE_SCN_MEM_READ, IMAGE_SCN_MEM_WRITE,
+    },
+};
 
 use crate::setup_linker;
 
@@ -11,4 +19,59 @@ fn externals_relaxation() {
         .build()
         .link()
         .expect("Could not link files");
+}
+
+#[test]
+fn no_merge_groups() {
+    let linked = setup_linker!("no_merge_groups.yaml", LinkerTargetArch::Amd64)
+        .merge_grouped_sections(false)
+        .build()
+        .link()
+        .expect("Could not link inputs");
+
+    let coff: CoffFile = CoffFile::parse(linked.as_slice()).expect("Could not parse linked output");
+
+    let test_sections: &[(&str, u32)] = &[
+        (
+            ".text$a",
+            IMAGE_SCN_CNT_CODE | IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_READ,
+        ),
+        (
+            ".text$b",
+            IMAGE_SCN_CNT_CODE | IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_READ,
+        ),
+        (
+            ".text$c",
+            IMAGE_SCN_CNT_CODE | IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_READ,
+        ),
+        (
+            ".data$a",
+            IMAGE_SCN_CNT_INITIALIZED_DATA | IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_WRITE,
+        ),
+        (
+            ".data$b",
+            IMAGE_SCN_CNT_INITIALIZED_DATA | IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_WRITE,
+        ),
+        (
+            ".data$c",
+            IMAGE_SCN_CNT_INITIALIZED_DATA | IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_WRITE,
+        ),
+    ];
+
+    for &(test_name, test_characteristics) in test_sections {
+        let section = coff
+            .section_by_name(test_name)
+            .unwrap_or_else(|| panic!("Could not find section {test_name}"));
+
+        let characteristics = section
+            .coff_section()
+            .characteristics
+            .get(object::LittleEndian);
+
+        assert!(
+            characteristics & test_characteristics == test_characteristics,
+            "{test_name} section has invalid characteristics. characteristics = {characteristics:#x?}, test_characteristics = {test_characteristics:#x?}, contained = {:#x?}",
+            characteristics & test_characteristics
+        );
+    }
 }
