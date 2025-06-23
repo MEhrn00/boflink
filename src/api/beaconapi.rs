9fbe9ca31266d30dde859907d1e5f86994885cfd
Matt Ehrnschwender <matthewe2020@gmail.com>
2025-06-22 22:14:12 -0400

    refactor: change Beacon API handling
    Load Beacon API symbols into a HashMap instead of abstracting as a trait

--- src/api/beaconapi.rs
+++ src/api/beaconapi.rs
@@ -1,127 +1,192 @@
-use std::path::Path;
-
-use object::Architecture;
+use std::collections::HashMap;
 
 use crate::{
-    libsearch::LibraryFind,
-    linker::{ApiInit, ApiInitCtx, error::ApiInitError},
+    linker::LinkerTargetArch,
     linkobject::import::{ImportMember, ImportName, ImportType},
 };
 
-use super::{ApiSymbolError, ApiSymbolSource};
-
-/// The Beacon API symbol string values.
-///
-/// Symbols are sorted based on commonality.
-const BEACONAPI_SYMBOLS: [&str; 52] = [
-    "BeaconPrintf",
-    "BeaconDataParse",
-    "BeaconOutput",
-    "BeaconDataExtract",
-    "BeaconDataInt",
-    "BeaconGetSpawnTo",
-    "BeaconCleanupProcess",
-    "BeaconSpawnTemporaryProcess",
-    "BeaconDataShort",
-    "toWideChar",
-    "BeaconUseToken",
-    "BeaconGetValue",
-    "BeaconRemoveValue",
-    "BeaconInjectProcess",
-    "BeaconDataLength",
-    "BeaconAddValue",
-    "BeaconRevertToken",
-    "BeaconOpenThread",
-    "BeaconUnmapViewOfFile",
-    "BeaconFormatInt",
-    "BeaconGetSyscallInformation",
-    "BeaconDataStoreProtectItem",
-    "BeaconFormatFree",
-    "BeaconDataStoreUnprotectItem",
-    "BeaconInformation",
-    "BeaconDataStoreMaxEntries",
-    "BeaconDuplicateHandle",
-    "BeaconOpenProcess",
-    "BeaconDataStoreGetItem",
-    "BeaconEnableBeaconGate:",
-    "BeaconVirtualQuery",
-    "BeaconWriteProcessMemory",
-    "BeaconSetThreadContext",
-    "BeaconVirtualProtect",
-    "BeaconFormatAppend",
-    "BeaconDisableBeaconGate",
-    "BeaconResumeThread",
-    "BeaconDataPtr",
-    "BeaconGetThreadContext",
-    "BeaconIsAdmin",
-    "BeaconVirtualAlloc",
-    "BeaconCloseHandle",
-    "BeaconReadProcessMemory",
-    "BeaconFormatReset",
-    "BeaconVirtualAllocEx",
-    "BeaconFormatPrintf",
-    "BeaconFormatToString",
-    "BeaconInjectTemporaryProcess",
-    "BeaconVirtualFree",
-    "BeaconGetCustomUserData",
-    "BeaconVirtualProtectEx",
-    "BeaconFormatAlloc",
+const BEACONAPI64_SYMBOLS: &[(&str, &str)] = &[
+    ("BeaconAddValue", "__imp_BeaconAddValue"),
+    ("BeaconCleanupProcess", "__imp_BeaconCleanupProcess"),
+    ("BeaconCloseHandle", "__imp_BeaconCloseHandle"),
+    ("BeaconDataExtract", "__imp_BeaconDataExtract"),
+    ("BeaconDataInt", "__imp_BeaconDataInt"),
+    ("BeaconDataLength", "__imp_BeaconDataLength"),
+    ("BeaconDataParse", "__imp_BeaconDataParse"),
+    ("BeaconDataPtr", "__imp_BeaconDataPtr"),
+    ("BeaconDataShort", "__imp_BeaconDataShort"),
+    ("BeaconDataStoreGetItem", "__imp_BeaconDataStoreGetItem"),
+    (
+        "BeaconDataStoreMaxEntries",
+        "__imp_BeaconDataStoreMaxEntries",
+    ),
+    (
+        "BeaconDataStoreProtectItem",
+        "__imp_BeaconDataStoreProtectItem",
+    ),
+    (
+        "BeaconDataStoreUnprotectItem",
+        "__imp_BeaconDataStoreUnprotectItem",
+    ),
+    ("BeaconDisableBeaconGate", "__imp_BeaconDisableBeaconGate"),
+    ("BeaconDuplicateHandle", "__imp_BeaconDuplicateHandle"),
+    ("BeaconEnableBeaconGate:", "__imp_BeaconEnableBeaconGate:"),
+    ("BeaconFormatAlloc", "__imp_BeaconFormatAlloc"),
+    ("BeaconFormatAppend", "__imp_BeaconFormatAppend"),
+    ("BeaconFormatFree", "__imp_BeaconFormatFree"),
+    ("BeaconFormatInt", "__imp_BeaconFormatInt"),
+    ("BeaconFormatPrintf", "__imp_BeaconFormatPrintf"),
+    ("BeaconFormatReset", "__imp_BeaconFormatReset"),
+    ("BeaconFormatToString", "__imp_BeaconFormatToString"),
+    ("BeaconGetCustomUserData", "__imp_BeaconGetCustomUserData"),
+    ("BeaconGetSpawnTo", "__imp_BeaconGetSpawnTo"),
+    (
+        "BeaconGetSyscallInformation",
+        "__imp_BeaconGetSyscallInformation",
+    ),
+    ("BeaconGetThreadContext", "__imp_BeaconGetThreadContext"),
+    ("BeaconGetValue", "__imp_BeaconGetValue"),
+    ("BeaconInformation", "__imp_BeaconInformation"),
+    ("BeaconInjectProcess", "__imp_BeaconInjectProcess"),
+    (
+        "BeaconInjectTemporaryProcess",
+        "__imp_BeaconInjectTemporaryProcess",
+    ),
+    ("BeaconIsAdmin", "__imp_BeaconIsAdmin"),
+    ("BeaconOpenProcess", "__imp_BeaconOpenProcess"),
+    ("BeaconOpenThread", "__imp_BeaconOpenThread"),
+    ("BeaconOutput", "__imp_BeaconOutput"),
+    ("BeaconPrintf", "__imp_BeaconPrintf"),
+    ("BeaconReadProcessMemory", "__imp_BeaconReadProcessMemory"),
+    ("BeaconRemoveValue", "__imp_BeaconRemoveValue"),
+    ("BeaconResumeThread", "__imp_BeaconResumeThread"),
+    ("BeaconRevertToken", "__imp_BeaconRevertToken"),
+    ("BeaconSetThreadContext", "__imp_BeaconSetThreadContext"),
+    (
+        "BeaconSpawnTemporaryProcess",
+        "__imp_BeaconSpawnTemporaryProcess",
+    ),
+    ("BeaconUnmapViewOfFile", "__imp_BeaconUnmapViewOfFile"),
+    ("BeaconUseToken", "__imp_BeaconUseToken"),
+    ("BeaconVirtualAlloc", "__imp_BeaconVirtualAlloc"),
+    ("BeaconVirtualAllocEx", "__imp_BeaconVirtualAllocEx"),
+    ("BeaconVirtualFree", "__imp_BeaconVirtualFree"),
+    ("BeaconVirtualProtect", "__imp_BeaconVirtualProtect"),
+    ("BeaconVirtualProtectEx", "__imp_BeaconVirtualProtectEx"),
+    ("BeaconVirtualQuery", "__imp_BeaconVirtualQuery"),
+    ("BeaconWriteProcessMemory", "__imp_BeaconWriteProcessMemory"),
+    ("toWideChar", "__imp_toWideChar"),
 ];
 
-/// Container for looking up Beacon API symbols.
-pub struct BeaconApiSymbols {
-    architecture: Architecture,
-    symbols: [&'static str; 52],
-}
+const BEACONAPI32_SYMBOLS: &[(&str, &str)] = &[
+    ("_BeaconAddValue", "__imp__BeaconAddValue"),
+    ("_BeaconCleanupProcess", "__imp__BeaconCleanupProcess"),
+    ("_BeaconCloseHandle", "__imp__BeaconCloseHandle"),
+    ("_BeaconDataExtract", "__imp__BeaconDataExtract"),
+    ("_BeaconDataInt", "__imp__BeaconDataInt"),
+    ("_BeaconDataLength", "__imp__BeaconDataLength"),
+    ("_BeaconDataParse", "__imp__BeaconDataParse"),
+    ("_BeaconDataPtr", "__imp__BeaconDataPtr"),
+    ("_BeaconDataShort", "__imp__BeaconDataShort"),
+    ("_BeaconDataStoreGetItem", "__imp__BeaconDataStoreGetItem"),
+    (
+        "_BeaconDataStoreMaxEntries",
+        "__imp__BeaconDataStoreMaxEntries",
+    ),
+    (
+        "_BeaconDataStoreProtectItem",
+        "__imp__BeaconDataStoreProtectItem",
+    ),
+    (
+        "_BeaconDataStoreUnprotectItem",
+        "__imp__BeaconDataStoreUnprotectItem",
+    ),
+    ("_BeaconDisableBeaconGate", "__imp__BeaconDisableBeaconGate"),
+    ("_BeaconDuplicateHandle", "__imp__BeaconDuplicateHandle"),
+    ("_BeaconEnableBeaconGate:", "__imp__BeaconEnableBeaconGate:"),
+    ("_BeaconFormatAlloc", "__imp__BeaconFormatAlloc"),
+    ("_BeaconFormatAppend", "__imp__BeaconFormatAppend"),
+    ("_BeaconFormatFree", "__imp__BeaconFormatFree"),
+    ("_BeaconFormatInt", "__imp__BeaconFormatInt"),
+    ("_BeaconFormatPrintf", "__imp__BeaconFormatPrintf"),
+    ("_BeaconFormatReset", "__imp__BeaconFormatReset"),
+    ("_BeaconFormatToString", "__imp__BeaconFormatToString"),
+    ("_BeaconGetCustomUserData", "__imp__BeaconGetCustomUserData"),
+    ("_BeaconGetSpawnTo", "__imp__BeaconGetSpawnTo"),
+    (
+        "_BeaconGetSyscallInformation",
+        "__imp__BeaconGetSyscallInformation",
+    ),
+    ("_BeaconGetThreadContext", "__imp__BeaconGetThreadContext"),
+    ("_BeaconGetValue", "__imp__BeaconGetValue"),
+    ("_BeaconInformation", "__imp__BeaconInformation"),
+    ("_BeaconInjectProcess", "__imp__BeaconInjectProcess"),
+    (
+        "_BeaconInjectTemporaryProcess",
+        "__imp__BeaconInjectTemporaryProcess",
+    ),
+    ("_BeaconIsAdmin", "__imp__BeaconIsAdmin"),
+    ("_BeaconOpenProcess", "__imp__BeaconOpenProcess"),
+    ("_BeaconOpenThread", "__imp__BeaconOpenThread"),
+    ("_BeaconOutput", "__imp__BeaconOutput"),
+    ("_BeaconPrintf", "__imp__BeaconPrintf"),
+    ("_BeaconReadProcessMemory", "__imp__BeaconReadProcessMemory"),
+    ("_BeaconRemoveValue", "__imp__BeaconRemoveValue"),
+    ("_BeaconResumeThread", "__imp__BeaconResumeThread"),
+    ("_BeaconRevertToken", "__imp__BeaconRevertToken"),
+    ("_BeaconSetThreadContext", "__imp__BeaconSetThreadContext"),
+    (
+        "_BeaconSpawnTemporaryProcess",
+        "__imp__BeaconSpawnTemporaryProcess",
+    ),
+    ("_BeaconUnmapViewOfFile", "__imp__BeaconUnmapViewOfFile"),
+    ("_BeaconUseToken", "__imp__BeaconUseToken"),
+    ("_BeaconVirtualAlloc", "__imp__BeaconVirtualAlloc"),
+    ("_BeaconVirtualAllocEx", "__imp__BeaconVirtualAllocEx"),
+    ("_BeaconVirtualFree", "__imp__BeaconVirtualFree"),
+    ("_BeaconVirtualProtect", "__imp__BeaconVirtualProtect"),
+    ("_BeaconVirtualProtectEx", "__imp__BeaconVirtualProtectEx"),
+    ("_BeaconVirtualQuery", "__imp__BeaconVirtualQuery"),
+    (
+        "_BeaconWriteProcessMemory",
+        "__imp__BeaconWriteProcessMemory",
+    ),
+    ("_toWideChar", "__imp__toWideChar"),
+];
 
-impl BeaconApiSymbols {
-    /// Returns the Beacon API symbols for the specified architecture.
-    pub fn new(arch: Architecture) -> BeaconApiSymbols {
-        Self {
-            architecture: arch,
-            symbols: BEACONAPI_SYMBOLS,
-        }
-    }
-}
+pub fn symbols(architecture: LinkerTargetArch) -> HashMap<&'static str, ImportMember<'static>> {
+    let symbol_names = if architecture == LinkerTargetArch::I386 {
+        BEACONAPI32_SYMBOLS
+    } else {
+        BEACONAPI64_SYMBOLS
+    };
 
-impl<'a> ApiSymbolSource<'a> for BeaconApiSymbols {
-    fn api_path(&self) -> &std::path::Path {
-        Path::new("Beacon API")
-    }
+    let mut symbol_map = HashMap::with_capacity(symbol_names.len() * 2);
+    symbol_map.extend(symbol_names.iter().map(|&(symbol, _)| {
+        (
+            symbol,
+            ImportMember {
+                architecture: architecture.into(),
+                symbol,
+                dll: "Beacon API",
+                import: ImportName::Name(symbol),
+                typ: ImportType::Code,
+            },
+        )
+    }));
 
-    fn extract_api_symbol(&self, symbol: &'a str) -> Result<ImportMember<'a>, ApiSymbolError> {
-        let unprefixed_symbol = if self.architecture == Architecture::I386 {
-            symbol.trim_start_matches("__imp__")
-        } else {
-            symbol.trim_start_matches("__imp_")
-        };
+    symbol_map.extend(symbol_names.iter().map(|&(symbol, import_symbol)| {
+        (
+            import_symbol,
+            ImportMember {
+                architecture: architecture.into(),
+                symbol,
+                dll: "Beacon API",
+                import: ImportName::Name(symbol),
+                typ: ImportType::Code,
+            },
+        )
+    }));
 
-        self.symbols
-            .iter()
-            .copied()
-            .find_map(|contained_symbol| {
-                (contained_symbol == unprefixed_symbol).then_some(ImportMember {
-                    architecture: self.architecture,
-                    symbol: contained_symbol,
-                    dll: "Beacon API",
-                    import: ImportName::Name(contained_symbol),
-                    typ: ImportType::Code,
-                })
-            })
-            .ok_or(ApiSymbolError::NotFound)
-    }
-}
-
-pub struct BeaconApiInit;
-
-impl ApiInit for BeaconApiInit {
-    type Output<'a> = BeaconApiSymbols;
-
-    #[inline]
-    fn initialize_api<'a, L: LibraryFind>(
-        &self,
-        ctx: &ApiInitCtx<'_, 'a, L>,
-    ) -> Result<Self::Output<'a>, ApiInitError> {
-        Ok(BeaconApiSymbols::new(ctx.target_arch.into()))
-    }
+    symbol_map
 }
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,7 +1,84 @@
 mod beaconapi;
-mod error;
-mod traits;
 
-pub use beaconapi::*;
-pub use error::*;
-pub use traits::*;
+use std::{collections::HashMap, path::Path};
+
+use log::warn;
+
+use crate::{
+    linker::LinkerTargetArch,
+    linkobject::{
+        archive::{ArchiveMemberError, ArchiveSymbolError, LinkArchive, LinkArchiveMemberVariant},
+        import::ImportMember,
+    },
+};
+
+#[derive(Debug, thiserror::Error)]
+pub enum ApiSymbolsError {
+    #[error("{0}")]
+    Symbol(#[from] ArchiveSymbolError),
+
+    #[error("{0}")]
+    Member(#[from] ArchiveMemberError),
+}
+
+pub struct ApiSymbols<'a> {
+    /// The custom API archive path if these symbols are from a custom API.
+    archive_path: &'a Path,
+
+    /// The symbols
+    symbols: HashMap<&'a str, ImportMember<'a>>,
+}
+
+impl<'a> ApiSymbols<'a> {
+    /// Creates a new [`ApiSymbols`] but using the Beacon API symbols.
+    pub fn beacon(architecture: LinkerTargetArch) -> ApiSymbols<'a> {
+        ApiSymbols {
+            archive_path: Path::new("BEACONAPI"),
+            symbols: beaconapi::symbols(architecture),
+        }
+    }
+
+    /// Returns the archive path for the API symbols.
+    pub fn archive_path(&self) -> &'a Path {
+        self.archive_path
+    }
+
+    /// Creates a new [`ApiSymbols`] from a [`LinkArchive`].
+    pub fn new(
+        path: &'a Path,
+        archive: LinkArchive<'a>,
+    ) -> Result<ApiSymbols<'a>, ApiSymbolsError> {
+        let symbol_iter = archive.symbols();
+
+        let symbol_count = symbol_iter.size_hint();
+        let mut symbols = HashMap::with_capacity(symbol_count.1.unwrap_or(symbol_count.0));
+
+        for symbol in symbol_iter {
+            let symbol = symbol?;
+            let (member_path, member) = symbol.extract()?;
+
+            match member {
+                LinkArchiveMemberVariant::Import(import_member) => {
+                    symbols.insert(symbol.name(), import_member);
+                }
+                LinkArchiveMemberVariant::Coff(_) => {
+                    warn!(
+                        "{}: ignoring COFF member '{}' in --custom-api",
+                        path.display(),
+                        member_path.display()
+                    );
+                }
+            }
+        }
+
+        Ok(ApiSymbols {
+            archive_path: path,
+            symbols,
+        })
+    }
+
+    /// Gets the [`ImportMember`] associated with the specified symbol.
+    pub fn get(&self, symbol: impl AsRef<str>) -> Option<&ImportMember<'a>> {
+        self.symbols.get(symbol.as_ref())
+    }
+}
