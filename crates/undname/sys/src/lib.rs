412747f595d7ccc4a946474934500665a2b22ae9
Matt Ehrnschwender <matthewe2020@gmail.com>
2025-07-14 22:10:42 -0400

    fix: linking to `__unDNameEx` for Windows targets

--- crates/undname/sys/src/lib.rs
+++ crates/undname/sys/src/lib.rs
@@ -1,9 +1,40 @@
-#![allow(non_camel_case_types)]
+#![allow(non_camel_case_types, non_snake_case)]
 
 pub type malloc_func_t = unsafe extern "C" fn(usize) -> *mut std::ffi::c_void;
 pub type free_func_t = unsafe extern "C" fn(*mut std::ffi::c_void);
 
-windows_link::link!("vcruntime140.dll" "C" fn __unDNameEx(buffer: *mut std::ffi::c_char, mangled: *const std::ffi::c_char, buflen: std::ffi::c_int, memget: malloc_func_t, memfree: free_func_t, unknown: *const std::ffi::c_void, flags: std::ffi::c_uint) -> *mut std::ffi::c_char);
+#[cfg(windows)]
+#[cfg_attr(target_env = "msvc", link(name = "vcruntime", kind = "dylib"))]
+#[cfg_attr(target_env = "gnu", link(name = "msvcrt", kind = "dylib"))]
+#[cfg_attr(
+    not(any(target_env = "msvc", target_env = "gnu")),
+    link(name = "VCRUNTIME140", kind = "raw-dylib")
+)]
+unsafe extern "C" {
+    pub fn __unDNameEx(
+        buffer: *mut std::ffi::c_char,
+        mangled: *const std::ffi::c_char,
+        buflen: std::ffi::c_int,
+        memget: malloc_func_t,
+        memfree: free_func_t,
+        unknown: *const std::ffi::c_void,
+        flags: std::ffi::c_uint,
+    ) -> *mut std::ffi::c_char;
+}
+
+#[cfg(not(windows))]
+#[allow(clippy::missing_safety_doc, unused_variables)]
+pub unsafe extern "C" fn __unDNameEx(
+    buffer: *mut std::ffi::c_char,
+    mangled: *const std::ffi::c_char,
+    buflen: std::ffi::c_int,
+    memget: malloc_func_t,
+    memfree: free_func_t,
+    unknown: *const std::ffi::c_void,
+    flags: std::ffi::c_uint,
+) -> *mut std::ffi::c_char {
+    unimplemented!("__unDNameEx is not implemented for non-Windows targets");
+}
 
 pub use windows_sys::Win32::System::Diagnostics::Debug::{
     UNDNAME_32_BIT_DECODE, UNDNAME_COMPLETE, UNDNAME_NAME_ONLY, UNDNAME_NO_ACCESS_SPECIFIERS,
