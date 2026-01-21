42a08f28404eee78b0503a55d5c5224388b0cbaa
Matt Ehrnschwender <matthewe2020@gmail.com>
2026-01-20 19:12:18 -0500

    fix: git hash embedding in release builds
    Git version information would not get embedded correctly in CI release
    builds due to a detached head checkout. This changes the sourcing
    logic to account for that

--- build.rs
+++ build.rs
@@ -1,29 +1,23 @@
-use std::process::Command;
-
 fn emit_githash() -> std::io::Result<()> {
-    let git_version = Command::new("git").arg("--version").spawn()?.wait()?;
-
-    if !git_version.success() {
-        return Err(std::io::Error::other("git --version exited unsuccessfully"));
-    }
-
-    let git_head = std::fs::read_to_string(".git/HEAD")?;
-
-    let git_ref = git_head
-        .split_once(" ")
-        .map(|(_, head_ref)| head_ref)
-        .ok_or_else(|| std::io::Error::other("could not get git HEAD ref"))?;
-
-    let git_shorthash_output = Command::new("git")
-        .args(["rev-parse", "--short", "HEAD"])
-        .output()?;
-
-    let shorthash = std::str::from_utf8(git_shorthash_output.stdout.as_slice())
-        .map_err(std::io::Error::other)?;
-
-    println!("cargo::rustc-env=GIT_SHORT_HASH={shorthash}");
+    let git_head = std::fs::read_to_string(".git/HEAD")?.trim().to_string();
     println!("cargo::rerun-if-changed=.git/HEAD");
-    println!("cargo::rerun-if-changed=.git/{git_ref}");
+
+    let githash = if let Some(head_ref) = git_head.strip_prefix("ref: ") {
+        let head_ref = format!(".git/{head_ref}");
+        let githash = std::fs::read_to_string(&head_ref)?;
+        println!("cargo::rerun-if-changed={head_ref}");
+        githash
+    } else {
+        git_head
+    };
+
+    let hashval = githash.trim();
+    let shorthash = if hashval.len() == 40 {
+        &hashval[..7]
+    } else {
+        "unknown"
+    };
+    println!("cargo::rustc-env=GIT_SHORT_HASH={shorthash}");
 
     Ok(())
 }
@@ -31,6 +25,6 @@ fn emit_githash() -> std::io::Result<()> {
 fn main() {
     println!("cargo::rerun-if-changed=build.rs");
     if emit_githash().is_err() {
-        println!("cargo::rustc-env=GIT_SHORT_HASH=unknown");
+        println!("cargo::rustc-env=GIT_SHORT_HASH=unknown")
     }
 }
