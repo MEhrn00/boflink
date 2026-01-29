150df4276a392ce9831cc3e5c046c571021e4931
Matt Ehrnschwender <matthewe2020@gmail.com>
2026-01-28 21:43:17 -0500

    refactor: remove unused coffyaml crate
    Crate was for building test data but is no longer used. Also remove the
    `objs2yaml` crate since it was a small cli wrapper for coffyaml

--- crates/coffyaml/src/importlib/errors.rs
+++
@@ -1,7 +0,0 @@
-use super::Architecture;
-
-#[derive(Debug, thiserror::Error)]
-pub enum ImportlibYamlBuildError {
-    #[error("architecture {0:?} is not supported")]
-    UnsupportArchitecture(Architecture),
-}
