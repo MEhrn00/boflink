d82e2f86abf63389c1ffbd0257dca8ba44b3cfd9
Matt Ehrnschwender <matthewe2020@gmail.com>
2026-01-28 23:25:50 -0500

    feat: normalize library search paths
    Removes '..' and '.' to make files found through search paths clearer
    in diagnostic messages

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,4 +1,8 @@
-use std::{collections::BTreeMap, ffi::OsString, path::PathBuf};
+use std::{
+    collections::BTreeMap,
+    ffi::OsString,
+    path::{Component, Path, PathBuf},
+};
 
 use boflink::linker::LinkerTargetArch;
 use clap::{ArgAction, ArgMatches, CommandFactory, FromArgMatches, Parser, ValueEnum};
@@ -371,6 +375,13 @@ pub fn parse_arguments() -> anyhow::Result<ParsedCliArgs> {
 
     setup_logging(&options);
 
+    // Lexically normalize library search paths
+    options.library_paths = options
+        .library_paths
+        .into_iter()
+        .map(lexically_normalize_path)
+        .collect::<Vec<_>>();
+
     if std::env::args_os().any(|arg| arg == "--color" || arg.starts_with("--color=")) {
         log::warn!(
             "'--color' is a deprecated alias for '--color-diagnostics' and will be removed in the next release"
@@ -393,4 +404,84 @@ fn setup_logging(options: &CliOptionArgs) {
 
     crate::logging::init(max_level, options.color_diagnostics, 0)
         .expect("logging should only be initialized once");
+}
+
+/// Lexically normalizes `path` using C++ `std::filesystem::path::lexically_normal()`
+/// normalization algorithm.
+///
+/// This normalization process will traverse up the filesystem which does
+/// not follow [`std::path::Path::normalize_lexically()`].
+fn lexically_normalize_path(path: impl AsRef<Path>) -> PathBuf {
+    let path = path.as_ref();
+    let mut iter = path.components().peekable();
+
+    let root = match iter.peek() {
+        None => return PathBuf::new(),
+        Some(p @ Component::RootDir) | Some(p @ Component::Prefix(_)) => Some(*p),
+        _ => None,
+    };
+
+    let mut normalized = PathBuf::new();
+
+    while let Some(component) = iter.next() {
+        match component {
+            Component::CurDir => continue,
+            Component::Normal(_) if iter.next_if_eq(&Component::ParentDir).is_some() => {
+                continue;
+            }
+            Component::ParentDir => {
+                if !normalized.pop() {
+                    if let Some(root) = root {
+                        normalized.push(root);
+                    } else {
+                        normalized.push(Component::ParentDir);
+                    }
+                }
+            }
+            o => {
+                normalized.push(o);
+            }
+        }
+    }
+
+    if normalized.components().next().is_none() {
+        normalized.push(Component::CurDir);
+    }
+
+    normalized
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    #[test]
+    fn lexically_normalize_path() {
+        let tests = [
+            ("", ""),
+            ("./", "."),
+            ("./foo", "foo"),
+            ("foo////////", "foo/"),
+            ("foo/./bar", "foo/bar"),
+            ("foo/./bar/..", "foo/"),
+            // Allow traversing up the filesystem
+            ("../foo", "../foo"),
+            ("foo/./bar/../../../baz", "../baz"),
+            // Check root normalization
+            ("/root/not/in/path/../../../foo/bar", "/root/foo/bar"),
+            ("/root/not/in/path///./../../../foo/bar", "/root/foo/bar"),
+            // New root
+            ("/root/foo/../../bar", "/bar"),
+        ];
+
+        for case in tests {
+            let test_path = Path::new(case.0);
+            let expected = Path::new(case.1);
+            assert_eq!(
+                super::lexically_normalize_path(test_path),
+                expected,
+                "case: {case:?}",
+            );
+        }
+    }
 }
